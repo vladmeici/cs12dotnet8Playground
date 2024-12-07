7f62d47a20b0334339c29f2f02e8dec866cf3031
Vlad Alexandru Meici <valexand@tibco.com>
2024-12-07 09:05:51 +0200

    Start Ch07

---
+++ Chapter06/WarningsAsErrors/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter a name: ");
+string? name = Console.ReadLine(); // Must declare the variable as nullable to remove the warning.
+if (name == null) return; // Must check for null to remove the warning.
+Console.WriteLine($"Hello, {name} has {name.Length} characters!");
---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Xml.Linq;
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
