20db6f765ab12146a49da5a579daeb85bd1fe004
Vlad Alexandru Meici <valexand@tibco.com>
2024-12-23 08:23:34 +0200

    Add ch08

---
+++ Chapter08/WorkingWithRegularExpressions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions; // To use Regex.
+
+Write("Enter your age: ");
+string input = ReadLine()!; // Null-forgiving operator.
+Regex ageChecker = DigitsOnly();
+WriteLine(ageChecker.IsMatch(input) ? "Thank you!" : $"This is not a valid age: {input}");
+
+// C# 1 to 10: Use escaped double-quotes characters \"
+// string films = "\"Monsters, Inc.\",\"I, Tonya\",\"Lock, Stock and Two Smoking Barrels\"";
+
+// C# 11 or later: Use """ to start and end a raw string literal
+string films = """
+"Monsters, Inc.","I, Tonya","Lock, Stock and Two Smoking Barrels"
+""";
+
+WriteLine($"Films to split: {films}");
+
+string[] filmsDumb = films.Split(',');
+
+WriteLine("Splitting with string.Split method:");
+foreach (string film in filmsDumb)
+{
+    WriteLine($"    {film}");
+}
+
+Regex csv = CommaSeparator();
+
+MatchCollection filmSmart = csv.Matches(films);
+
+WriteLine("Splitting with regular expression:");
+foreach (Match film in filmSmart)
+{
+    WriteLine($"    {film.Groups[2].Value}");
+}
---
+++ Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization; // To use CultureInfo.
+
+OutputEncoding = System.Text.Encoding.UTF8; // Enable Euro symbol.
+
+string city = "London";
+WriteLine($"{city} is {city.Length} characters long");
+WriteLine($"First char is {city[0]} and fourth char is {city[3]}");
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellin";
+
+string[] citiesArray = cities.Split(',');
+
+WriteLine($"There are {citiesArray.Length} items in the array:");
+
+foreach (string item in citiesArray)
+{
+    WriteLine($"    {item}");
+}
+
+string fullName = "Alan Shore";
+
+int indexOfTheSpace = fullName.IndexOf(' ');
+
+string firstName = fullName.Substring(startIndex: 0, length: indexOfTheSpace);
+string lastName = fullName.Substring(startIndex: indexOfTheSpace + 1);
+
+WriteLine($"Original: {fullName}");
+WriteLine($"Swapped: {lastName}, {firstName}");
+
+string company = "Microsoft";
+WriteLine($"Text: {company}");
+WriteLine("Starts with M: {0}, contains an N: {1}", arg0: company.StartsWith("M"), arg1: company.Contains("N"));
+
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+string text1 = "Mark";
+string text2 = "MARK";
+
+WriteLine($"text1: {text1}, text2: {text2}");
+
+WriteLine("Compare: {0}.", string.Compare(text1, text2));
+
+WriteLine("Compare (ignoreCase): {0}.", string.Compare(text1, text2, ignoreCase: true));
+
+WriteLine("Compare (InvariantCultureIgnoreCase): {0}.", string.Compare(text1, text2, StringComparison.InvariantCultureIgnoreCase));
+
+string recombined = string.Join(" => ", citiesArray);
+WriteLine(recombined);
+
+string fruit = "Apples";
+decimal price = 0.39M;
+DateTime when = DateTime.Now;
+
+WriteLine($"Interpolated: {fruit} cost {price:C} on {when:dddd}.");
+WriteLine(string.Format("string.Format: {0} cost {1:C} on {2:dddd}.", arg0: fruit, arg1: price, arg2: when));
