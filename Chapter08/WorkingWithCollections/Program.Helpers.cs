7997e39cf4b19d0c80cbe075c9bc35aab6929e68
Vlad Alexandru Meici <valexand@tibco.com>
2024-12-24 07:54:00 +0200

    Collections:

---
+++ Chapter08/WorkingWithCollections/Program.Helpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+partial class Program
+{
+    private static void OutputCollection<T>(string title, IEnumerable<T> collection)
+    {
+        WriteLine($"{title}:");
+        foreach (T item in collection)
+        {
+            WriteLine($"    {item}");
+        }
+    }
+
+    private static void OutputPQ<TElement, TPriority>(string title,
+        IEnumerable<(TElement Element, TPriority Priority)> collection)
+    {
+        WriteLine($"{title}:");
+        foreach ((TElement, TPriority) item in collection)
+        {
+            WriteLine($"    {item.Item1}: {item.Item2}");
+        }
+    }
+}
